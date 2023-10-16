b65a05cfd003c7b7ac1db51b031cb910f3e0f40f
SonyaBorovskaya <80105695+SonyaBorovskaya@users.noreply.github.com>
2023-10-16 16:30:58 +0300

    Delete studiaDent directory

--- studiaDent/login/includes/logout.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-header("Location: ../../pages/index.php");
-// header("Location:http://localhost/DiplomKs/pages/index.php");
-// header("Location: ../indexL.php");
